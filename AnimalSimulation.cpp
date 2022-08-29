0873973f1bf5d6ea5125f19faa7f708b69cce7a4
Hoja Makarov <Philosopher@DESKTOP-3A6VMMG>
2022-08-29 07:53:38 -0700

    Random movement and Map rendering

--- AnimalSimulation.cpp
+++ AnimalSimulation.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <conio.h>
+#include <chrono>
 #include <vector>
 #include <array>
 
@@ -38,6 +40,48 @@ enum PLANT_STATE { BEGINING_LIFE, MIDPART_LIFE, ENDING_LIFE };
 // ** Состояние у каждой сущности?
 
 // Базовые классы
+class CoordinateSystem
+{
+	int x;
+	int y;
+
+public:
+	CoordinateSystem(int x, int y) : x(y), y(x) {}
+
+	int get_x() { return x; }
+	int get_y() { return y; }
+};
+
+class Map
+{
+	int x_size;
+	int y_size;
+
+public:
+	Map(int x, int y)
+	{
+		CoordinateSystem cs = CoordinateSystem(x, y);
+
+		x_size = cs.get_x();
+		y_size = cs.get_y();
+	}
+
+	bool is_position_valid(int x, int y)
+	{
+		return (x >= 0 && x < x_size) &&
+			(y >= 0 && y < y_size);
+	}
+
+	bool is_position_valid(CoordinateSystem cs)
+	{
+		return (cs.get_x() >= 0 && cs.get_x() < x_size) &&
+			(cs.get_y() >= 0 && cs.get_y() < y_size);
+	}
+
+	int get_xsize() { return x_size; }
+	int get_ysize() { return y_size; }
+};
+
 class Entity
 {
 	int id;
@@ -55,14 +99,19 @@ public:
 	int get_x() { return x; }
 	int get_y() { return y; }
 
-	void set_x(int x) { x = x; }
-	void set_y(int y) { y = y; }
+	void set_x(int x) { this->x = x; }
+	void set_y(int y) { this->y = y; }
 
 	virtual bool is_animal() { return false; }
 	virtual bool is_food() { return false; }
 	virtual bool is_plant() { return false; }
 
-	virtual bool move_to(int x, int y) { return false; }
+	virtual bool move_to(Map& map, int x, int y) { return false; }
+	virtual bool move_up(Map& map) { return false; }
+	virtual bool move_down(Map& map) { return false; }
+	virtual bool move_right(Map& map) { return false; }
+	virtual bool move_left(Map& map) { return false; }
+
 	virtual bool update_state() { return false; }
 };
 
@@ -72,24 +121,6 @@ class State
 
 public:
 	State(short int state) : state(state) {}
-};
-
-class Map
-{
-	int x_size;
-	int y_size;
-
-public:
-	Map(int x, int y) : x_size(x), y_size(y) {}
-
-	bool is_position_valid(int x, int y)
-	{
-		return (x >= 1 && x <= x_size) &&
-			(y >= 1 && y <= y_size);
-	}
-
-	int get_xsize() { return x_size; }
-	int get_ysize() { return y_size; }
 };
 
 // Состояния
@@ -146,12 +177,34 @@ public:
 
 	bool is_animal() override { return true; }
 	bool is_predator() { return is_predator_type; }
-	bool move_to(int x, int y) override
+	bool move_to(Map& map, int x, int y) override
 	{
+		CoordinateSystem cs = CoordinateSystem(x, y);
+
+		if (!map.is_position_valid(cs))
+			return false;
+
 		set_x(x);
 		set_y(y);
 
 		return true;
+	}
+
+	bool move_up(Map &map) override
+	{
+		return move_to(map, get_x(), get_y() + 1);
+	}
+	bool move_down(Map& map) override
+	{
+		return move_to(map, get_x(), get_y() - 1);
+	}
+	bool move_right(Map& map) override
+	{
+		return move_to(map, get_x() + 1, get_y());
+	}
+	bool move_left(Map& map) override
+	{
+		return move_to(map, get_x() - 1, get_y());
 	}
 };
 
@@ -197,8 +250,8 @@ public:
 		entities = std::vector<Entity *>(a_count + f_count + p_count);
 		for (int i = 0; i < animals_count; i++)
 		{
-			Entity* ent = new Animal(last_id, rand() % map.get_xsize() + 1,
-				rand() % map.get_ysize() + 1,
+			Entity* ent = new Animal(last_id, rand() % map.get_xsize(),
+				rand() % map.get_ysize(),
 				(bool)(rand() % 2),
 				3, 0, AnimalState(AnimalState::get_moving()));
 
@@ -209,8 +262,8 @@ public:
 
 		for (int i = 0; i < food_count; i++)
 		{
-			Entity* ent = new Food(last_id, rand() % map.get_xsize() + 1,
-				rand() % map.get_ysize() + 1,
+			Entity* ent = new Food(last_id, rand() % map.get_xsize(),
+				rand() % map.get_ysize(),
 				1, FoodState(FoodState::get_fresh()));
 
 			entities[i + animals_count] = ent;
@@ -220,8 +273,8 @@ public:
 
 		for (int i = 0; i < plants_count; i++)
 		{
-			Entity* ent = new Plant(last_id, rand() % map.get_xsize() + 1,
-				rand() % map.get_ysize() + 1,
+			Entity* ent = new Plant(last_id, rand() % map.get_xsize(),
+				rand() % map.get_ysize(),
 				3, PlantState(PlantState::get_begin()));
 
 			entities[i + animals_count + food_count] = ent;
@@ -239,6 +292,7 @@ class View
 {
 	Model& model;
 	std::string map_representation;
+	char** positions;
 
 public:
 	View(Model& model) : model(model)
@@ -248,7 +302,7 @@ public:
 
 		map_representation = "";
 
-		char** positions = new char* [x_dim];
+		positions = new char* [x_dim];
 
 		for (int i = 0; i < x_dim; i++)
 		{
@@ -257,7 +311,6 @@ public:
 			for (int j = 0; j < y_dim; j++)
 				positions[i][j] = '-';
 		}
-
 
 		std::vector<Entity *> entities = model.get_entities();
 
@@ -282,7 +335,7 @@ public:
 			else
 				symbol = 'X';
 
-			positions[entities[i]->get_x() - 1][entities[i]->get_y() - 1] = symbol;
+			positions[entities[i]->get_x()][entities[i]->get_y()] = symbol;
 		}
 
 		for (int i = 0; i < x_dim; i++)
@@ -294,12 +347,65 @@ public:
 
 			map_representation += "\n";
 		}
+	}
+
+	~View()
+	{
+		for (int i = 0; i < model.get_map().get_xsize(); i ++)
+			delete positions[i];
+
+		delete positions;
+	}
+
+	void update()
+	{
+		int x_dim = model.get_map().get_xsize();
+		int y_dim = model.get_map().get_ysize();
+
+		map_representation = "";
+
+		for (int i = 0; i < x_dim; i++)
+			for (int j = 0; j < y_dim; j++)
+				positions[i][j] = '-';
+
+		std::vector<Entity*> entities = model.get_entities();
+
+		for (int i = 0; i < entities.size(); i++)
+		{
+			char symbol = ' ';
+
+			if (entities[i]->is_animal())
+			{
+				Animal* animal = dynamic_cast<Animal*>(entities[i]);
+
+				if (animal->is_predator())
+					symbol = '&';
+
+				else
+					symbol = '@';
+			}
+
+			else if (entities[i]->is_food())
+				symbol = '*';
+
+			else
+				symbol = 'X';
+
+			positions[entities[i]->get_x()][entities[i]->get_y()] = symbol;
+		}
 
 		for (int i = 0; i < x_dim; i++)
 		{
-			delete positions[i];
+			for (int j = 0; j < y_dim; j++)
+			{
+				map_representation += std::string(1, positions[i][j]) + " ";
+			}
+
+			map_representation += "\n";
 		}
 	}
+
+	void render() { std::cout << get_representation(); }
 
 	std::string get_representation()
 	{
@@ -307,13 +413,98 @@ public:
 	}
 };
 
+class Control
+{
+	Model& model;
+	View& view;
+
+public:
+	Control(Model& model, View& view) : model(model), view(view)
+	{
+		std::vector<Entity*> entities = model.get_entities();
+		auto last_time = std::chrono::system_clock::now();
+
+		while (true)
+		{
+			auto now = std::chrono::system_clock::now();
+
+			if (std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time).count() < 400)
+			{
+				while (_kbhit())
+					_getch();
+
+				continue;
+			}
+
+			 //(!(_kbhit() && _getch() == 13))
+
+			last_time = now;
+
+			system("cls");
+
+			for (auto entity : entities)
+			{
+				if (!entity->is_animal())
+				continue;
+
+				int direction = rand() % 4;
+
+				Map map = model.get_map();
+
+				switch (direction)
+				{
+				case 0:
+					entity->move_up(map);
+					break;
+
+				case 1:
+					entity->move_down(map);
+					break;
+
+				case 2:
+					entity->move_left(map);
+					break;
+
+				case 3:
+					entity->move_right(map);
+					break;
+				}
+			}
+
+			view.update();
+			view.render();
+
+			while (true)
+			{
+				int key = -1;
+
+				if (_kbhit())
+					key = _getch();
+
+				if (key == 27)
+					exit(0);
+
+				else if (key == 13)
+					break;
+
+			}
+		}
+	}
+};
+
 int main()
 {
-	Model model = Model(70, 100, 10, 5, 6);
+	//while (true)
+	//{
+	//	if (_kbhit())
+	//		std::cout << _getch() << "\n";
+	//}
+
+	Model model = Model(30, 30, 10, 5, 6);
 
 	View view = View(model);
 
-	std::cout << view.get_representation();
+	Control control = Control(model, view);
 
 	return 0;
 }
